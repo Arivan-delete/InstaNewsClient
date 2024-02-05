b8324578114ba09c81a95d8e2803cee7682e16d2
Artem <artem.mamedov5375@gmail.com>
2024-02-05 17:04:52 +0200

    Post Card

--- app/src/main/java/com/example/instagramnewsclient/MainActivity.kt
+++ app/src/main/java/com/example/instagramnewsclient/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.instagramnewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.instagramnewsclient.ui.theme.InstagramNewsClientTheme
+import com.example.instagramnewsclient.ui.theme.PostCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,29 +23,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstagramNewsClientTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
                 ) {
-                    Greeting("Android")
+                    PostCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstagramNewsClientTheme {
-        Greeting("Android")
     }
 }
