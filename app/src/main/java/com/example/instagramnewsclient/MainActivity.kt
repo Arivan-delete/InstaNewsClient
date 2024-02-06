28657912f4fb61366a4b171bc4a76e65cfd47fb5
Artem <artem.mamedov5375@gmail.com>
2024-02-06 18:19:40 +0200

    Scaffold

--- app/src/main/java/com/example/instagramnewsclient/MainActivity.kt
+++ app/src/main/java/com/example/instagramnewsclient/MainActivity.kt
@@ -7,12 +7,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramnewsclient.ui.theme.InstagramNewsClientTheme
 import com.example.instagramnewsclient.ui.theme.PostCard
@@ -21,17 +33,77 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramNewsClientTheme {
-                // A surface container using the 'background' color from the theme
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(8.dp)
-                ) {
-                    PostCard()
+            Test()
+//            InstagramNewsClientTheme {
+//                // A surface container using the 'background' color from the theme
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(MaterialTheme.colorScheme.background)
+//                        .padding(8.dp)
+//                ) {
+//                    PostCard()
+//                }
+//            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Test() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "TopAppBar title")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, contentDescription = "")
+                    }
                 }
+            )
+        },
+
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Filled.Favorite, contentDescription = "")
+                    },
+                    label = {
+                        Text(text = "Favorite")
+                    }
+                )
+                NavigationBarItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Outlined.Edit, contentDescription = "")
+                    },
+                    label = {
+                        Text(text = "Edit")
+                    }
+                )
+                NavigationBarItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Outlined.Add, contentDescription = "")
+                    },
+                    label = {
+                        Text(text = "Add")
+                    }
+                )
             }
         }
+    ) {
+        Text(
+            text = "This is Scaffold",
+            modifier = Modifier.padding(it)
+        )
     }
 }
